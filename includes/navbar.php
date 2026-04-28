7c63f4d9788b54a6c822a74e66f2a9648078555d
shanzaofficial512-sys <shanzaofficial512@gmail.com>
2026-04-28 13:40:09 +0500

    updated navbar design

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,62 +1,74 @@
+<?php
+$currentPage = basename($_SERVER['PHP_SELF']);
+
+// Dropdown groups define karo
+$pagesGroup = ['contact.php', 'pdfnotes.php', 'GKmcqs.php', 'Islammcqs.php', 'english.php', 'math.php', 'submitmcqs.php'];
+$submitGroup = ['ppsc.php', 'fpsc.php'];
+$affairsGroup = ['current-affairs.php'];
+
+// Check karo kaunsa group active hai
+$pagesActive   = in_array($currentPage, $pagesGroup)  ? 'active' : '';
+$submitActive  = in_array($currentPage, $submitGroup)  ? 'active' : '';
+$affairsActive = in_array($currentPage, $affairsGroup) ? 'active' : '';
+?>
+
 <!-- ============ Navbar Start ============ -->
 <nav class="navbar">
 
-    <!-- Logo -->
-    <div class="logo">LOGO</div>
+  <!-- Logo -->
+  <div class="logo">LOGO</div>
 
-    <!-- Hamburger button (mobile ke liye) -->
-    <div class="hamburger" id="hamburgerBtn">
-        &#9776;
-    </div>
+  <!-- Hamburger button -->
+  <div class="hamburger" id="hamburgerBtn">&#9776;</div>
 
-    <!-- Menu links -->
-    <ul class="nav-menu" id="navMenu">
+  <!-- Menu links -->
+  <ul class="nav-menu" id="navMenu">
 
-        <li><a href="index.php" class="active">Home</a></li>
-        <li><a href="about.php">About Us</a></li>
+    <li><a href="index.php" class="<?= $currentPage == 'index.php' ? 'active' : '' ?>">Home</a></li>
+    <li><a href="about.php" class="<?= $currentPage == 'about.php' ? 'active' : '' ?>">About Us</a></li>
 
-        <!-- Pages dropdown -->
-        <li class="dropdown">
-            <a href="#">Pages &#9662;</a>
-            <ul class="dropdown-menu">
-                 <li><a href="contact.php">Contact</a></li> 
-                 <li><a href="pdfnotes.php">PDF Notes</a></li>
-                 <li><a href="GKmcqs.php">GK Mcqs</a></li>
-                 <li><a href="submitmcqs.php">Submit Mcqs</a></li>
-                 <li><a href="islammcqs.php">Islamic Studies</a></li>
-                 <li><a href="english.php">English</a></li>
-                 <li><a href="math.php">Mathematics</a></li>
-            </ul>
-        </li>
+    <!-- Pages dropdown -->
+    <li class="dropdown">
+      <a href="#" class="<?= $pagesActive ?>">Pages &#9662;</a>
+      <ul class="dropdown-menu">
+        <li><a href="contact.php"    class="<?= $currentPage == 'contact.php'    ? 'active' : '' ?>">Contact</a></li>
+        <li><a href="pdfnotes.php"   class="<?= $currentPage == 'pdfnotes.php'   ? 'active' : '' ?>">PDF Notes</a></li>
+        <li><a href="GKmcqs.php"     class="<?= $currentPage == 'GKmcqs.php'     ? 'active' : '' ?>">GK Mcqs</a></li>
+        <li><a href="Islammcqs.php"  class="<?= $currentPage == 'Islammcqs.php'  ? 'active' : '' ?>">Islamic Studies</a></li>
+        <li><a href="english.php"    class="<?= $currentPage == 'english.php'    ? 'active' : '' ?>">English</a></li>
+        <li><a href="math.php"       class="<?= $currentPage == 'math.php'       ? 'active' : '' ?>">Mathematics</a></li>
+      </ul>
+    </li>
 
-        <!-- Submit Mcqs dropdown -->
-        <li class="dropdown">
-            <a href="#">Submit Mcqs &#9662;</a>
-            <ul class="dropdown-menu">
-                <li><a href="ppsc.php">PPSC Past Papers</a></li>
-                <li><a href="fpsc.php">FPSC Mcqs</a></li>
-            </ul>
-        </li>
+    <!-- Submit Mcqs dropdown -->
+    <li class="dropdown">
+      <a href="#" class="<?= $submitActive ?>">Submit Mcqs &#9662;</a>
+      <ul class="dropdown-menu">
+        <li><a href="ppsc.php" class="<?= $currentPage == 'ppsc.php' ? 'active' : '' ?>">PPSC Past Papers</a></li>
+        <li><a href="fpsc.php" class="<?= $currentPage == 'fpsc.php' ? 'active' : '' ?>">FPSC Mcqs</a></li>
+      </ul>
+    </li>
 
-        <!-- Current Affairs dropdown -->
-        <li class="dropdown">
-            <a href="#">Current Affairs &#9662;</a>
-            <ul class="dropdown-menu">
-                <li><a href="current-affairs.php">National</a></li>
-                <li><a href="current-affairs.php">International</a></li>
-                <li><a href="current-affairs.php">Sports</a></li>
-            </ul>
-        </li>
 
-        <!-- Search icon -->
-        <li>
-            <a href="#" class="search-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#c5a30a" viewBox="0 0 24 24">
-                    <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-                </svg>
-            </a>
-        </li>
+    <!-- Current Affairs dropdown -->
+    <li class="dropdown">
+      <a href="#" class="<?= $affairsActive ?>">Current Affairs &#9662;</a>
+      <ul class="dropdown-menu">
+        <li><a href="current-affairs.php" class="<?= $currentPage == 'current-affairs.php' ? 'active' : '' ?>">National</a></li>
+        <li><a href="current-affairs.php">International</a></li>
+        <li><a href="current-affairs.php">Sports</a></li>
+      </ul>
+    </li>
 
-    </ul>
+    <!-- Search icon -->
+    <li>
+      <a href="#" class="search-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#c5a30a" viewBox="0 0 24 24">
+          <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+        </svg>
+      </a>
+    </li>
+
+  </ul>
 </nav>
 <!-- ============ Navbar End ============ -->
